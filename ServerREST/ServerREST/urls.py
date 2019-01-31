9c82c7cf835c0e90aef1859b78d3c5ec7bfb5157
MaxenceDecanter <44zrgqTB8XGP>
2019-01-31 11:02:41 +0100

    Ajout url

--- ServerREST/ServerREST/urls.py
+++ ServerREST/ServerREST/urls.py
@@ -17,8 +17,11 @@ from django.conf.urls import url
 from django.contrib import admin
 from rest_framework.urlpatterns import format_suffix_patterns
 
+from server import views
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^LogoSearch/$', views.ServerList.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
