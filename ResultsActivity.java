754c74ea121aa1f4eb098f8a7f0c30d10bf92dc4
Saga-Chan <louise.crampon@gmail.com>
2019-01-31 13:39:11 +0100

    Code application Android
    Version 1 avant insertion de la notion d'appareil photo permettant le scan.
    31/01/2019

---
+++ ResultsActivity.java
@@ -0,0 +1,11 @@
+package com.example.louis.picture_browser;
+
+import android.os.Bundle;
+
+public class ResultsActivity extends PictureBrowser{
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.results_page);
+    }
+}
