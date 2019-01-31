cdf660157c3657feabfaa0872c38c54783b0ac97
MaxenceDecanter <44zrgqTB8XGP>
2019-01-31 09:53:41 +0100

    Création models

--- ServerREST/server/models.py
+++ ServerREST/server/models.py
@@ -3,4 +3,15 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
 # Create your models here.
+
+class ResponseServer(models.Model):
+    result = models.URLField()
+    score = models.DecimalField(decimal_places=2, max_digits=10)
+
+
+class ImageSearch(models.Model):
+    date = models.DateTimeField(auto_now_add=True)
+    client = models.CharField(max_length=100)
+    response = models.OneToOneField(ResponseServer, on_delete=models.CASCADE, primary_key=True)
