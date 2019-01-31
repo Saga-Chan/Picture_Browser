31767f21946e886f93f6dfb1269a2b34fd1a3212
MaxenceDecanter <44zrgqTB8XGP>
2019-01-31 10:45:14 +0100

    Modification serializers

--- ServerREST/server/serializers.py
+++ ServerREST/server/serializers.py
@@ -5,13 +5,7 @@ from server.models import ResponseServer
 from server.models import ImageSearch
 
 
-class ResponseServerSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ResponseServer
-        fields = ('result', 'score')
-
-
 class ImageSearchSerializer(serializers.ModelSerializer):
     class Meta:
         model = ImageSearch
-        fields = ('date', 'client', 'response')
+        fields = ('date', 'client', 'response', 'score')
