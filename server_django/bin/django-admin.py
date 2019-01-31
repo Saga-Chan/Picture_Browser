6209797c720b294d0a7110c841b6a4bffb327b3c
MaxenceDecanter <44zrgqTB8XGP>
2019-01-31 09:41:13 +0100

    Initialisation

---
+++ server_django/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/decanter/PycharmProjects/Picture_Browser/server_django/bin/python2
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
